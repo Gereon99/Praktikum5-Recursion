3d3ed74fba74bf873d772a9fde389c03aa8314ef
Gereon Varel <gereon.varel@fh-muenster.de>
2019-12-10 18:26:25 +0100

    Initial commit.

---
+++ cmake-build-debug/CMakeFiles/QueenProblem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QueenProblem.dir/main.c.o"
+  "QueenProblem.pdb"
+  "QueenProblem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/QueenProblem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
