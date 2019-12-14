11d6959809675dcd6fa2176cc3282cdf9a4a8b87
Gereon <gereon.varel@gmail.com>
2019-12-14 02:36:47 +0100

    Implementation of the correct 'notThreatened method'

--- main.c
+++ main.c
@@ -1,62 +1,85 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define DIM 8
 
 int board[DIM];
 int solutions = 0;
 
-int notThreatened(int column, int row) {
-    // return 1 if queen can be placed in column
-    // Left
-    for (int i = column; i > 0; i--) {
-        if (row == board[i - 1]) {
-            return 0;
-        }
-    }
-    // Diagonal Top Left
-    for (int i = column; i > 0; i--) {
-        if (row == board[i - 1] + 1) {
-            return 0;
-        }
-    }
-
-    // Diagonal Bottom Left
-    for (int i = column; i > 0; i--) {
-        if (row == board[i - 1] - 1) {
-            return 0;
-        }
-    }
-    // No Threat
-    return 1;
-}
-
 void printSolution() {
-    printf("Solution found.\n\n");
+    // Constructing a simple chess board
+    printf("%i   ", DIM);
+    for (int i = 0; i < DIM; i++) {
+        printf("%i  ", i + 1);
+    }
+    printf("\n  o");
+    for (int i = 0; i < DIM; i++) {
+        printf("- -");
+    }
+    printf("o\n");
+    for (int i = 0; i < DIM; i++) {
+        printf("%i |", i + 1);
+        for (int j = 0; j < DIM; j++) {
+            if (board[i] == j) {
+                printf(" X ");
+            } else {
+                printf(" . ");
+            }
+        }
+        printf("|\n");
+    }
+    printf("  o");
+    for (int i = 0; i < DIM; i++) {
+        printf("- -");
+    }
+    printf("o\n\n");
     solutions++;
 }
 
+int notThreatened(int column, int row) {
+    // Check left side
+    for (int i = column; i > 0; i--) {
+        int previousColumn = i - 1;
+        if (column != 0 && row == board[previousColumn]) {
+            return 0;
+        }
+    }
+    // Check top-left diagonal
+    for (int i = 1, j = row; i <= column && j > 0; i++, j--) {
+        if (column != 0 && row == board[column - i] + i) {
+            return 0;
+        }
+    }
+    // Check bottom-left diagonal
+    for (int i = 1, j = row; i <= column && j < DIM; i++, j++) {
+        if (column != 0 && row == board[column - i] - i) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void placeIn(int column) {
-    if (column == 8) {
-        printSolution();
-    } else {
+    // Keep placing queens until every combination was successfully checked
+    if (column != DIM) {
         for (int row = 0; row < DIM; row++) {
             if (notThreatened(column, row)) {
                 board[column] = row;
                 placeIn(column + 1);
             }
         }
-    };
+    } else {
+        // 'DIM' amount of queens have been placed
+        printSolution();
+    }
 }
 
 int main() {
     printf("Printing all solutions to the %i queens problem:\n\n", DIM);
     for (int i = 0; i < DIM; i++) {
-        board[i] = -10;
-        printf("%i ", board[i]);
+        board[i] = 0;
     }
-    printf("\n\n");
+    // Placing the first queen in the first column
     placeIn(0);
-    printf("Found a total of %i solutions.", solutions);
+    printf("Found a total of %i solutions.\n", solutions);
     return 0;
 }
